9727c7dbd521a92a94aa7b7a0730a91fb9f47b59
Dostovalov Igor <dostovalov.igor@gmail.com>
2017-12-24 23:53:12 +0300

    main

--- algoritmix/js/main.js
+++ algoritmix/js/main.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
-    $('.header__menu-toggler').click(function (e) {
-        $('.content-wrap, .header-side-nav').toggleClass('toggled');
-        e.preventDefault();
-    });
 
-    $('body').swipe({
+    if (window.matchMedia("(max-width: 1170px)").matches) {
+
+        $('.header__menu-toggler').click(function (e) {
+            $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
+        });
+
+        $('body').swipe({
 //            swipeLeft:function() {
 //                $('.content-wrap, .header-side-nav').toggleClass('toggled');
 //            },
-        swipeRight:function() {
-            $('.content-wrap, .header-side-nav').toggleClass('toggled');
-        }
-    });
-
-    $('.content-wrap').swipe({
-        tap:function() {
-            if($('.content-wrap').hasClass('toggled')) {
-                $('.content-wrap, .header-side-nav').toggleClass('toggled');
+            swipeRight: function () {
+                $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
             }
-        },
-    });
+        });
+
+        $('.content-wrap').swipe({
+            tap: function () {
+                if ($('.content-wrap').hasClass('toggled')) {
+                    $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
+                }
+            },
+        });
+    }
 
 });
