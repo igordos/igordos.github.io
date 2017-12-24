054467ff5704e42db94bbf223c1418183aa6344b
Dostovalov Igor <dostovalov.igor@gmail.com>
2017-12-25 00:42:46 +0300

    main

--- algoritmix/js/main.js
+++ algoritmix/js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-
-    if (window.matchMedia("(max-width: 1170px)").matches) {
-
+    if (window.matchMedia('(max-width: 768px)').matches) {
         $('.header__menu-toggler').click(function (e) {
-            $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
+            $('.content-wrap, .header-side-nav').toggleClass('toggled');
+            e.preventDefault();
         });
 
         $('body').swipe({
@@ -11,17 +10,16 @@ $(document).ready(function () {
 //                $('.content-wrap, .header-side-nav').toggleClass('toggled');
 //            },
             swipeRight: function () {
-                $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
+                $('.content-wrap, .header-side-nav').toggleClass('toggled');
             }
         });
 
         $('.content-wrap').swipe({
             tap: function () {
                 if ($('.content-wrap').hasClass('toggled')) {
-                    $('html, .content-wrap, .header-side-nav').toggleClass('toggled');
+                    $('.content-wrap, .header-side-nav').toggleClass('toggled');
                 }
             },
         });
     }
-
 });
