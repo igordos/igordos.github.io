19d5abf9294e6718229d6eb12cf8c4bdf7a4439c
idostovalov <dostovalov.igor@gmail.com>
2018-10-31 08:00:36 +0300

    add video

--- escape/js/main.js
+++ escape/js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
 
-    // currentQuestion = 1;
-    // questionList = $worksheetForm.data('question-list');
-    // style = $worksheetForm.data('style');
-    // $worksheetQuestion.html('');
-    // $worksheetBtn.html('');
-    // $worksheetForm.html(renderWorksheet());
-    // $('#worksheet-wrap, #worksheet-question').toggleClass('toggled');
-
     // Run worksheet
     $('.page-service__btn').click(function (e) {
         // Set current question for btn click
@@ -24,11 +16,35 @@ $(document).ready(function () {
     $('.header-menu-btn').click(function (e) {
         $('.menu').toggleClass('toggled ' + 'menu_' + $(this).data('menu'));
         $('body, .header-menu-btn').toggleClass('toggled');
+
+        if ($('.contact').hasClass('toggled')) {
+            $('.contact').removeClass('toggled');
+            overlayContact = false;
+        }
+
+        if ($('.header-menu-btn').hasClass('toggled')) {
+            overlayMenu = true; // var init in lescroll.min.js
+        } else {
+            overlayMenu = false;
+        }
+
     });
 
     // Open contact
     $('.header-contact').click(function (e) {
-        $('body, .contact').toggleClass('toggled');
+        $('body, .contact, .header-contact').toggleClass('toggled');
+
+        if ($('.menu, .header-menu-btn').hasClass('toggled')) {
+            $('.menu, .header-menu-btn').removeClass('toggled');
+            overlayMenu = false;
+        }
+
+        if ($('.header-contact').hasClass('toggled')) {
+            overlayContact = true; // var init in lescroll.min.js
+        } else {
+            overlayContact = false;
+        }
+
     });
 
     // Initial config for modals contact form
@@ -93,5 +109,184 @@ $(document).ready(function () {
 
     });
 
-
 });
+
+// Google map
+
+let googleMapStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#bdbdbd"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e5e5e5"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dadada"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e5e5e5"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c9c9c9"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+        ]
+    }
+];
+
+let map;
+
+function initMap() {
+    let latLng = {lat: 55.7782821, lng: 37.5710249};
+    map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+        center: latLng,
+        zoom: 14,
+        styles: googleMapStyle,
+    });
+    let marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'ESCAPE',
+    });
+}
+
