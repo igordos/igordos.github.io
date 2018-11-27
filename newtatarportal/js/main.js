9af0915b10e3f14490130055ce90f213aaf4e792
idostovalov <dostovalov.igor@gmail.com>
2018-11-27 18:56:48 +0300

    ask

--- newtatarportal/js/main.js
+++ newtatarportal/js/main.js
@@ -115,32 +115,16 @@ $(document).ready(function () {
         $('body, .record-booking, #recordNavBtn').toggleClass('toggled');
     });
 
-    // Tabs
+    // Ask librarian
+    $('#askNavBtn').click(function () {
+        $(this).toggleClass('toggled');
+        $('body, .ask-facet, .ask-mask').toggleClass('toggled');
+    });
 
-    // store tabs variable
-    let myTabs = document.querySelectorAll("ul.nav-tabs > li");
-
-    function myTabClicks(tabClickEvent) {
-        for (let i = 0; i < myTabs.length; i++) {
-            myTabs[i].classList.remove("active");
-        }
-        let clickedTab = tabClickEvent.currentTarget;
-        clickedTab.classList.add("active");
-        tabClickEvent.preventDefault();
-        let myContentPanes = document.querySelectorAll(".nav-tab-panel");
-        for (i = 0; i < myContentPanes.length; i++) {
-            myContentPanes[i].classList.remove("active");
-        }
-        let anchorReference = tabClickEvent.target;
-        let activePaneId = anchorReference.getAttribute("href");
-        let activePane = document.querySelector(activePaneId);
-        activePane.classList.add("active");
-    }
-
-    for (i = 0; i < myTabs.length; i++) {
-        myTabs[i].addEventListener("click", myTabClicks)
-    }
-
+    $('.ask-mask').click(function () {
+        $(this).toggleClass('toggled');
+        $('body, .ask-facet, #askNavBtn').toggleClass('toggled');
+    });
 
 });
 
